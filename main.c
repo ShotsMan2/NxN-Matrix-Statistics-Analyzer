3c9856b5f1f239e783816c8d235b4b55ee427eac
Selim Efe <uysalselimefe@gmail.com>
2026-01-16 12:58:56 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,47 +4,55 @@
 
 int main()
 {
-int n,i,j,matris[99][99],min,max,sum,ort;
+    int n, i, j, matris[99][99], min, max, sum, ort;
 
     srand(time(NULL));
 
     printf("n i giriniz ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            matris[i][j]=rand()%101;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            matris[i][j] = rand() % 101;
         }
     }
 
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            printf("%d \t",matris[i][j]);
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%d \t", matris[i][j]);
         }
         printf("\n");
     }
 
-    min=matris[0][0];
-    max=matris[0][0];
-    sum=0;
+    min = matris[0][0];
+    max = matris[0][0];
+    sum = 0;
 
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            if(matris[i][j]<min){
-                min=matris[i][j];
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (matris[i][j] < min)
+            {
+                min = matris[i][j];
             }
-            if(matris[i][j]>max){
-                max=matris[i][j];
+            if (matris[i][j] > max)
+            {
+                max = matris[i][j];
             }
-            sum+=matris[i][j];
+            sum += matris[i][j];
         }
     }
 
-    ort=sum/(n*n);
+    ort = sum / (n * n);
 
     printf("\n");
 
-    printf("min: %d \n",min);
-    printf("max: %d \n",max);
-    printf("ort: %d \n",ort);
+    printf("min: %d \n", min);
+    printf("max: %d \n", max);
+    printf("ort: %d \n", ort);
 }
